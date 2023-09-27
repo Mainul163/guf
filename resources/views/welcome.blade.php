8939a480479f42e9af11f0f64c8e36664687302d
unknown <mainulisraf163@gmail.com>
2023-09-27 17:52:46 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,640 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>GUF</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+    /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+    html {
+        line-height: 1.15;
+        -webkit-text-size-adjust: 100%
+    }
+
+    body {
+        margin: 0
+    }
+
+    a {
+        background-color: transparent
+    }
+
+    [hidden] {
+        display: none
+    }
+
+    html {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+        line-height: 1.5
+    }
+
+    *,
+    :after,
+    :before {
+        box-sizing: border-box;
+        border: 0 solid #e2e8f0
+    }
+
+    a {
+        color: inherit;
+        text-decoration: inherit
+    }
+
+    svg,
+    video {
+        display: block;
+        vertical-align: middle
+    }
+
+    video {
+        max-width: 100%;
+        height: auto
+    }
+
+    .bg-white {
+        --bg-opacity: 1;
+        background-color: #fff;
+        background-color: rgba(255, 255, 255, var(--bg-opacity))
+    }
+
+    .bg-gray-100 {
+        --bg-opacity: 1;
+        background-color: #f7fafc;
+        background-color: rgba(247, 250, 252, var(--bg-opacity))
+    }
+
+    .border-gray-200 {
+        --border-opacity: 1;
+        border-color: #edf2f7;
+        border-color: rgba(237, 242, 247, var(--border-opacity))
+    }
+
+    .border-t {
+        border-top-width: 1px
+    }
+
+    .flex {
+        display: flex
+    }
+
+    .grid {
+        display: grid
+    }
+
+    .hidden {
+        display: none
+    }
+
+    .items-center {
+        align-items: center
+    }
+
+    .justify-center {
+        justify-content: center
+    }
+
+    .font-semibold {
+        font-weight: 600
+    }
+
+    .h-5 {
+        height: 1.25rem
+    }
+
+    .h-8 {
+        height: 2rem
+    }
+
+    .h-16 {
+        height: 4rem
+    }
+
+    .text-sm {
+        font-size: .875rem
+    }
+
+    .text-lg {
+        font-size: 1.125rem
+    }
+
+    .leading-7 {
+        line-height: 1.75rem
+    }
+
+    .mx-auto {
+        margin-left: auto;
+        margin-right: auto
+    }
+
+    .ml-1 {
+        margin-left: .25rem
+    }
+
+    .mt-2 {
+        margin-top: .5rem
+    }
+
+    .mr-2 {
+        margin-right: .5rem
+    }
+
+    .ml-2 {
+        margin-left: .5rem
+    }
+
+    .mt-4 {
+        margin-top: 1rem
+    }
+
+    .ml-4 {
+        margin-left: 1rem
+    }
+
+    .mt-8 {
+        margin-top: 2rem
+    }
+
+    .ml-12 {
+        margin-left: 3rem
+    }
+
+    .-mt-px {
+        margin-top: -1px
+    }
+
+    .max-w-6xl {
+        max-width: 72rem
+    }
+
+    .min-h-screen {
+        min-height: 100vh
+    }
+
+    .overflow-hidden {
+        overflow: hidden
+    }
+
+    .p-6 {
+        padding: 1.5rem
+    }
+
+    .py-4 {
+        padding-top: 1rem;
+        padding-bottom: 1rem
+    }
+
+    .px-6 {
+        padding-left: 1.5rem;
+        padding-right: 1.5rem
+    }
+
+    .pt-8 {
+        padding-top: 2rem
+    }
+
+    .fixed {
+        position: fixed
+    }
+
+    .relative {
+        position: relative
+    }
+
+    .top-0 {
+        top: 0
+    }
+
+    .right-0 {
+        right: 0
+    }
+
+    .shadow {
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)
+    }
+
+    .text-center {
+        text-align: center
+    }
+
+    .text-gray-200 {
+        --text-opacity: 1;
+        color: #edf2f7;
+        color: rgba(237, 242, 247, var(--text-opacity))
+    }
+
+    .text-gray-300 {
+        --text-opacity: 1;
+        color: #e2e8f0;
+        color: rgba(226, 232, 240, var(--text-opacity))
+    }
+
+    .text-gray-400 {
+        --text-opacity: 1;
+        color: #cbd5e0;
+        color: rgba(203, 213, 224, var(--text-opacity))
+    }
+
+    .text-gray-500 {
+        --text-opacity: 1;
+        color: #a0aec0;
+        color: rgba(160, 174, 192, var(--text-opacity))
+    }
+
+    .text-gray-600 {
+        --text-opacity: 1;
+        color: #718096;
+        color: rgba(113, 128, 150, var(--text-opacity))
+    }
+
+    .text-gray-700 {
+        --text-opacity: 1;
+        color: #4a5568;
+        color: rgba(74, 85, 104, var(--text-opacity))
+    }
+
+    .text-gray-900 {
+        --text-opacity: 1;
+        color: #1a202c;
+        color: rgba(26, 32, 44, var(--text-opacity))
+    }
+
+    .underline {
+        text-decoration: underline
+    }
+
+    .antialiased {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale
+    }
+
+    .w-5 {
+        width: 1.25rem
+    }
+
+    .w-8 {
+        width: 2rem
+    }
+
+    .w-auto {
+        width: auto
+    }
+
+    .grid-cols-1 {
+        grid-template-columns: repeat(1, minmax(0, 1fr))
+    }
+
+    @media (min-width:640px) {
+        .sm\:rounded-lg {
+            border-radius: .5rem
+        }
+
+        .sm\:block {
+            display: block
+        }
+
+        .sm\:items-center {
+            align-items: center
+        }
+
+        .sm\:justify-start {
+            justify-content: flex-start
+        }
+
+        .sm\:justify-between {
+            justify-content: space-between
+        }
+
+        .sm\:h-20 {
+            height: 5rem
+        }
+
+        .sm\:ml-0 {
+            margin-left: 0
+        }
+
+        .sm\:px-6 {
+            padding-left: 1.5rem;
+            padding-right: 1.5rem
+        }
+
+        .sm\:pt-0 {
+            padding-top: 0
+        }
+
+        .sm\:text-left {
+            text-align: left
+        }
+
+        .sm\:text-right {
+            text-align: right
+        }
+    }
+
+    @media (min-width:768px) {
+        .md\:border-t-0 {
+            border-top-width: 0
+        }
+
+        .md\:border-l {
+            border-left-width: 1px
+        }
+
+        .md\:grid-cols-2 {
+            grid-template-columns: repeat(2, minmax(0, 1fr))
+        }
+    }
+
+    @media (min-width:1024px) {
+        .lg\:px-8 {
+            padding-left: 2rem;
+            padding-right: 2rem
+        }
+    }
+
+    @media (prefers-color-scheme:dark) {
+        .dark\:bg-gray-800 {
+            --bg-opacity: 1;
+            background-color: #2d3748;
+            background-color: rgba(45, 55, 72, var(--bg-opacity))
+        }
+
+        .dark\:bg-gray-900 {
+            --bg-opacity: 1;
+            background-color: #1a202c;
+            background-color: rgba(26, 32, 44, var(--bg-opacity))
+        }
+
+        .dark\:border-gray-700 {
+            --border-opacity: 1;
+            border-color: #4a5568;
+            border-color: rgba(74, 85, 104, var(--border-opacity))
+        }
+
+        .dark\:text-white {
+            --text-opacity: 1;
+            color: #fff;
+            color: rgba(255, 255, 255, var(--text-opacity))
+        }
+
+        .dark\:text-gray-400 {
+            --text-opacity: 1;
+            color: #cbd5e0;
+            color: rgba(203, 213, 224, var(--text-opacity))
+        }
+
+        .dark\:text-gray-500 {
+            --tw-text-opacity: 1;
+            color: #6b7280;
+            color: rgba(107, 114, 128, var(--tw-text-opacity))
+        }
+    }
+    </style>
+
+    <link rel="stylesheet" href="{{asset('customTable/assets/vendor/fonts/boxicons.css')}}" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{asset('customTable/assets/vendor/css/core.css')}}"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{asset('customTable/assets/vendor/css/theme-default.css')}}"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{asset('customTable/assets/css/demo.css')}}" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{asset('customTable/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+
+    <link rel="stylesheet" href="{{asset('customTable/assets/vendor/libs/apex-charts/apex-charts.css')}}" />
+
+    <!-- Page CSS -->
+
+    <!-- poppin font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!-- Helpers -->
+    <script src="{{asset('customTable/assets/vendor/js/helpers.js')}}"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{asset('customTable/assets/js/config.js')}}"></script>
+    <style>
+    * {
+        font-family: 'Poppins', sans-serif;
+    }
+    </style>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+
+    <link rel="stylesheet" href="cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+
+    <!-- template -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+
+    <script src="{!!url('/js/jquery.min.js')!!}"></script>
+</head>
+
+<body class="antialiased">
+
+
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Forms/</span> Horizontal Layouts</h4>
+
+        <!-- Basic Layout & Basic with Icons -->
+        <div class="row">
+            <!-- Basic Layout -->
+            <div class="col-xxl">
+                <div class="card mb-4">
+
+                    <div class="card-body">
+                        <form>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-name">আবেদনকারী নাম</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="basic-default-name" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-company">আবেদনকারীর জাতীয়
+                                    পরিচয় পত্র</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="basic-default-company" />
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-company">মোবাইল
+                                    নাম্বার</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="basic-default-company" />
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-company">আবেদনকারীর
+                                    ঠিকানা</label>
+
+                            </div>
+
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-company ">জেলা</label>
+
+                                <div class="col-sm-10">
+                                    <!-- <label for="browser"></label> -->
+                                    <input list="browsers" name="browser" id="browser" class="form-select">
+
+                                    <datalist id="browsers">
+                                        <!-- <option value="আজকের দিন">
+                                        <option value="সকাল ">
+                                        <option value="বাতের ">
+                                        <option value="কাজের ">
+                                        <option value="যোগাযোগ "> -->
+                                        <option selected></option>
+                                        <option value="1"></option>
+                                        <option value="2"></option>
+                                        <option value="3"></option>
+                                    </datalist>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-email">Email</label>
+                                <div class="col-sm-10">
+                                    <div class="input-group input-group-merge">
+                                        <input type="text" id="basic-default-email" class="form-control"
+                                            placeholder="john.doe" aria-label="john.doe"
+                                            aria-describedby="basic-default-email2" />
+                                        <span class="input-group-text" id="basic-default-email2">@example.com</span>
+                                    </div>
+                                    <div class="form-text">You can use letters, numbers & periods</div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-phone">Phone No</label>
+                                <div class="col-sm-10">
+                                    <input type="text" id="basic-default-phone" class="form-control phone-mask"
+                                        placeholder="658 799 8941" aria-label="658 799 8941"
+                                        aria-describedby="basic-default-phone" />
+                                </div>
+                            </div>
+
+
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="basic-default-message">Message</label>
+                                <div class="col-sm-10">
+                                    <textarea id="basic-default-message" class="form-control"
+                                        placeholder="Hi, Do you have a moment to talk Joe?"
+                                        aria-label="Hi, Do you have a moment to talk Joe?"
+                                        aria-describedby="basic-icon-default-message2"></textarea>
+                                </div>
+                            </div>
+                            <div class="row justify-content-end">
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-primary">Send</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script>
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+    </script>
+
+
+
+    <script src="{{asset('customTable/assets/vendor/libs/jquery/jquery.js')}}"></script>
+    <script src="{{asset('customTable/assets/vendor/libs/popper/popper.js')}}"></script>
+    <script src="{{asset('customTable/assets/vendor/js/bootstrap.js')}}"></script>
+    <script src="{{asset('customTable/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+
+    <script src="{{asset('customTable/assets/vendor/js/menu.js')}}"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{asset('customTable/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+
+    <!-- Main JS -->
+    <script src="{{asset('customTable/assets/js/main.js')}}"></script>
+
+    <!-- Page JS -->
+    <script src="{{asset('customTable/assets/js/dashboards-analytics.js')}}"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+
+
+
+
+
+    <script src="cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="../../plugins/jszip/jszip.min.js"></script>
+    <script src="../../plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="../../plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <!-- <script src="../../dist/js/demo.js"></script> -->
+    <!-- Page specific script -->
+    <script>
+    < /body>
+
+    <
+    /html>
