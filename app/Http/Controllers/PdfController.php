82c9b91d015f83f6ca75deb0a8eec49958e3042d
unknown <mainulisraf163@gmail.com>
2023-10-26 18:18:54 +0600

    working

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -11,23 +11,47 @@ class PdfController extends Controller
     public function export_pdf(){
 
       
-        $gufData=DB::table('guf_info')->latest('id')->limit(1)->get();
-
-        $pdf = \PDF::loadView('guf.pdfFile',[
+        // $gufData=DB::table('guf_info')->latest('id')->limit(1)->get();
+     
+        // $pdf = \PDF::loadView('guf.pdfFile',[
           
-            'gufData'=>$gufData,
+        //     'gufData'=>$gufData,
           
           
-        ]) ->setOptions(['enable_php' => true])
-        ->setPaper('A4', 'portrait');
+        // ]) ->setOptions(['enable_php' => true])
+        // ->setPaper('A4', 'portrait');
 
        
-        // $pdf = PDF::loadView('communityDetails.pdf', compact('commun_details','communityDetailsTitle'))->setOptions(['defaultFont' => 'sans-serif']);
-        return $pdf->download('guf.pdfFile');
+        
+        // return $pdf->download('guf.pdf');
+
+  $defaultConfig=(new \Mpdf\config\ConfigVariables())->getDefaults();
+  $fontDirs=$defaultConfig['fontDir'];
+  $defaultFontConfig=(new \Mpdf\config\FontVariables())->getDefaults();
+  $fontData=$defaultFontConfig['fontdata'];
+  $path= public_path()."/fonts";
+  $mpdf = new \Mpdf\Mpdf([
+   
+    'format' => 'A4',
+    'orientation' => 'P',
+    'fontDir'=>array_merge($fontDirs,[$path]),
+    'fontdata'=>$fontData +[
+      'solaimanlipi'=>[
+        'R'=>'SolaimanLipi_20-04-07.ttf',
+        'useOTL'=>0xFF,
+        'useKashida' => 75, 
+      ],
+
+    ],
+    'default_font'=>'solaimanlipi'
+]) ;    
+
+ $mpdf->writeHTML(view('guf.pdfFile'));
+ $mpdf->Output('test.pdf','D');
 
 
 
-       
+
 
     }
 }
